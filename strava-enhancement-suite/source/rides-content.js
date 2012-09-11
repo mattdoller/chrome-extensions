c248c8eae49799fdf3b2f23a680436650cd92682
Matt Doller <mattdoller@gmail.com>
2012-09-10 23:16:31 -0400

    more work on loading up other riders that were on segments from a given ride

--- strava-enhancement-suite/source/rides-content.js
+++ strava-enhancement-suite/source/rides-content.js
@@ -1,12 +1,15 @@
 $(document).ready(function() {
 	
-	// add ride export
-	addGpxExportButton();
-	
-	function addGpxExportButton() {
+	var context = new StravaContext(window.location);
+	var rideId = context.rideId();
 
-		var context = new StravaContext(window.location);
-		var rideId = context.rideId();
+	// add ride export
+	addGpxExportButton(rideId);
+	
+	// add other riders also on this ride
+	addOtherRiders(rideId);
+
+	function addGpxExportButton(rideId) {
 	
 		if ($('#activity-actions').length != 0) {
 			console.log('We already have an activity actions collection on this page.  We must be on one of our own rides.  Skipping...');
@@ -26,6 +29,75 @@ $(document).ready(function() {
 			var blob = new Blob([gpx.xml], { type: 'application/xml+gpx' });
 			saveAs(blob, gpx.name + '.gpx');
 		});	
-	};
+	}
+
+	function addOtherRiders(rideId) {
+		
+		var strava = new StravaApi();
+
+		// load up the ride
+		var ride;
+		strava.findRide(rideId, {
+			success: function(response) {
+				ride = response.ride;
+			}
+		});
+
+		// find the ride efforts on this ride
+		strava.findRideEfforts(rideId, {
+			success: function(response) {
+
+				var efforts = response.efforts;
+
+				$.each(efforts, function(idx, effort) {
+					var segment = effort.segment;
+					loadSegment(segment, ride);
+				});
+
+			}
+		});
+
+
+		// for each segment effort, get the athlete on the effort itself
+
+		// take the id of the athlete, and generate a link to the athlete page
+
+		// add the link to the efforts table
+
+	}
+
+	function loadSegment(segment, ride) {
+		// look up segment efforts started on or after the ride
+		// date - 1, and completed before or after the end date
+		var strava = new StravaApi();
+		
+		// startDate is a date formatted as YYYY-MM-DDTHH:MM:SSZ
+		
+		// remove the trailing 'Z' from the string
+		var trimmed = ride.startDate.slice(0, ride.startDate.length - 1);
+		var startDate = Date.parse(trimmed);
+		var endDate = startDate.clone().add(1).days();
+
+		var data = {
+			startDate: startDate.toString('yyyy-MM-dd'),
+			endDate: endDate.toString('yyyy-MM-dd')
+		};
+
+		strava.findSegmentEfforts(segment.id, data, {
+			success: function(response) {
+				var efforts = response.efforts;
+				$.each(efforts, function(idx, effort) {
+					loadEffort(effort);
+				});
+			}
+		});
+	}
+	
+	function loadEffort(effort) {
+		var athlete = effort.athlete;
+		var url = StravaUrls.athleteUrl(athlete.id);
+		var name = athlete.name;
+		console.log('name: {0}, url: {1}'.format([name, url]));
+	}
 
 });
